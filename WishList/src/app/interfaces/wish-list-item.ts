e4841bf04c3bb4cfe218e7a276c3bd43984402e0
laure <laurencribb36@gmail.com>
2024-11-15 16:08:37 +0000

    added sort to price but can't change stars due to data source type instead of array

--- WishList/src/app/interfaces/wish-list-item.ts
+++ WishList/src/app/interfaces/wish-list-item.ts
@@ -1,7 +1,7 @@
 export interface WishListItem {
     id: number;
-    name: string;
     image?: string;
+    name: string;
     starRating: number;
     price?: number;
 }
--- WishList/src/app/wish-list-items/wish-list-items.component.ts
+++ WishList/src/app/wish-list-items/wish-list-items.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
 import { placeholderItems } from '../placeholder-items';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { NgFor } from '@angular/common';
 import { StarRatingDisplayComponent } from '../star-rating-display/star-rating-display.component';
 import { WishListItem } from '../interfaces/wish-list-item';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-wish-list-items',
   standalone: true,
-  imports: [NgFor, StarRatingDisplayComponent],
+  imports: [NgFor, MatTableModule, MatSortModule, StarRatingDisplayComponent],
   templateUrl: './wish-list-items.component.html',
   styleUrl: './wish-list-items.component.css'
 })
-export class WishListItemsComponent {
-  items: WishListItem[] = placeholderItems;
+export class WishListItemsComponent implements AfterViewInit{
+  private _liveAnnouncer = inject(LiveAnnouncer);
+  items = new MatTableDataSource(placeholderItems);;
+  // need to add star rating and # back in once sorting figured out
+  displayedColumns: string[] = ['#', 'Image', 'Name', 'price'];
   newRatingScore: number = 0;
 
-  setRating(newRating: number, i: number){
-    this.items[i].starRating = newRating 
+  
+  @ViewChild(MatSort) sort = new MatSort();
+
+  ngAfterViewInit() {
+    this.items.sort = this.sort;
   }
+  
+  // setRating(newRating: number, i: number){
+  //   this.items[i].starRating = newRating 
+  // }
+
 }
